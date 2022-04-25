709614e349f1ed066e3cae20b4d7c9582ca8cdff
sneakers-the-rat <JLSaunders987@gmail.com>
2022-04-25 14:30:40 -0700

    subject ingestion

--- python/wehrdj/ingest/colony.py
+++ python/wehrdj/ingest/colony.py
@@ -2,15 +2,16 @@
 Ingest the colony database into datajoint
 """
 import pandas as pd
-import typing
 from pathlib import Path
 
-from element_animal import subject, genotyping
+from element_animal import subject
+
+from wehrdj.ingest.utils import col_to_datetime, filter_nans
 
 MOUSE_DB_MAP = {
     'Unnamed: 0': 'subject',
     'Sex': 'sex',
-    'DOB': 'subject_birth',
+    'DOB': 'subject_birth_date',
     "Date Sac'd": "death_date",
     'Protocol': 'protocol'
 }
@@ -18,31 +19,16 @@ MOUSE_DB_MAP = {
 class MouseDB(pd.DataFrame):
     """Trivial subtype of dataframe to indicate this is a mouse db dataframe"""
 
-def col_to_datetime(column:pd.Series) -> pd.Series:
-    """Date column with improperly padded m/d/y formatting"""
-    # remove any nonnumeric, nonslash characters
-    column = column.str.replace(r'[^\d/]', '', regex=True)
-    split_date = column.str.split('/', expand=True).rename(columns={0: 'month', 1: "day", 2: "year"})
-    # zfill months and days
-    split_date['month'] = split_date['month'].str.zfill(2)
-    split_date['day'] = split_date['day'].str.zfill(2)
-    # strip preceding "20" on some dates
-    split_date['year'] = split_date['year'].str[-2:]
-    # recombine
-    date_join = split_date['year'] + '/' + split_date['month'] + '/' + split_date['day']
-    date = pd.to_datetime(date_join, format="%y/%m/%d", errors='coerce')
-    return date
-
-
-
-
 
 def load_mouse_db(path:Path) -> MouseDB:
+    """
+    Load the mouse database from a .csv export of the "Mice" sheet from the colony database
+    """
     df = MouseDB(pd.read_csv(path))
     # rename columns
     df = df.rename(columns=MOUSE_DB_MAP)
     # reformat date columns to strings
-    df['subject_birth'] = col_to_datetime(df['subject_birth'])
+    df['subject_birth_date'] = col_to_datetime(df['subject_birth_date'])
     df['death_date'] = col_to_datetime(df['death_date'])
 
     return df
@@ -50,4 +36,7 @@ def load_mouse_db(path:Path) -> MouseDB:
 
 
 def insert_subjects(mousedb:MouseDB):
-    subject.Subject.insert(mousedb[['subject', 'sex', 'subject_birth']])
+    mousedb = mousedb[['subject', 'sex', 'subject_birth_date']]
+    # filter nans
+    mousedb = filter_nans(mousedb)
+    subject.Subject.insert(mousedb, skip_duplicates=True)
---
+++ python/wehrdj/ingest/utils.py
@@ -0,0 +1,28 @@
+import warnings
+
+import pandas as pd
+
+
+def col_to_datetime(column:pd.Series) -> pd.Series:
+    """Date column with improperly padded m/d/y formatting"""
+    # remove any nonnumeric, nonslash characters
+    column = column.str.replace(r'[^\d/]', '', regex=True)
+    split_date = column.str.split('/', expand=True).rename(columns={0: 'month', 1: "day", 2: "year"})
+    # zfill months and days
+    split_date['month'] = split_date['month'].str.zfill(2)
+    split_date['day'] = split_date['day'].str.zfill(2)
+    # strip preceding "20" on some dates
+    split_date['year'] = split_date['year'].str[-2:]
+    # recombine
+    date_join = split_date['year'] + '/' + split_date['month'] + '/' + split_date['day']
+    date = pd.to_datetime(date_join, format="%y/%m/%d", errors='coerce')
+    return date
+
+
+def filter_nans(df:pd.DataFrame) -> pd.DataFrame:
+    """filter any rows with NANs with warning"""
+    na_rows = df.isna().any(axis=1)
+    if sum(na_rows) > 0:
+        warnings.warn(f"Dropping {sum(na_rows)} rows with NaNs in them:\n{df[na_rows]}")
+    df = df[~na_rows]
+    return df
